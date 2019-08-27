ee7839ca492f0a87d0ce64f92514b652e53bb2ac
chungwei <wei.chungwei@gmail.com>
2019-08-27 21:56:44 +0800

    add .gitignore

--- 面试题/toutiao.go
+++ 面试题/toutiao.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	n := []int{5, 1, 1, 2, 0, 0}
 	fmt.Println(qSort(n, 0, len(n)-1))
 	n = []int{5, 8, 1, 2, 0, 40}
 	fmt.Println(qSort(n, 0, len(n)-1))
+
+	n = []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	fmt.Println(rain(n))
 }
 
 func qSort(nums []int, left, right int) []int {
@@ -33,9 +38,41 @@ func qSort(nums []int, left, right int) []int {
 }
 
 func rain(height []int) int {
+	l := len(height)
+	if l < 3 {
+		return 0
+	}
 
+	sum, max := 0, 0
+	for k, v := range height {
+		if height[max] < v {
+			max = k
+		}
+	}
+
+	t := 0
+	for i := 0; i <= max; i++ {
+		if t <= height[i] {
+			t = height[i]
+		} else {
+			sum += t - height[i]
+		}
+	}
+
+	t = 0
+	for i := l - 1; i >= max; i-- {
+		if t <= height[i] {
+			t = height[i]
+		} else {
+			sum += t - height[i]
+		}
+	}
+
+	return sum
 }
 
-func candy() {
+func candy(ratings []int) int {
+	l := len(ratings)
 
+	return 0
 }
