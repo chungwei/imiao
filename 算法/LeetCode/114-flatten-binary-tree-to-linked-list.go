c22041ed8a601bb27ffdcdae8843acdd5427d597
chungwei <wei.chungwei@gmail.com>
2019-11-25 16:07:39 +0800

    update 114-flatten-binary-tree-to-linked-list.go

--- 算法/LeetCode/114-flatten-binary-tree-to-linked-list.go
+++ 算法/LeetCode/114-flatten-binary-tree-to-linked-list.go
@@ -34,10 +34,6 @@ import (
 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-func main() {
-	nums := []int{2, 7, 11, 15}
-	fmt.Println(twoSum(nums, 22))
-}
 
 /**
  * Definition for a binary tree node.
@@ -61,3 +57,79 @@ func flatten(root *TreeNode) {
 	root.Right = r
 
 }
+
+/**
+public void flatten(TreeNode root) {
+    while (root != null) {
+        //左子树为 null，直接考虑下一个节点
+        if (root.left == null) {
+            root = root.right;
+        } else {
+            // 找左子树最右边的节点
+            TreeNode pre = root.left;
+            while (pre.right != null) {
+                pre = pre.right;
+            }
+            //将原来的右子树接到左子树的最右边节点
+            pre.right = root.right;
+            // 将左子树插入到右子树的地方
+            root.right = root.left;
+            root.left = null;
+            // 考虑下一个节点
+            root = root.right;
+        }
+    }
+}
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+
+//将 1 的左子树插入到右子树的地方
+    1
+     \
+      2         5
+     / \         \
+    3   4         6
+//将原来的右子树接到左子树的最右边节点
+    1
+     \
+      2
+     / \
+    3   4
+         \
+          5
+           \
+            6
+
+ //将 2 的左子树插入到右子树的地方
+    1
+     \
+      2
+       \
+        3       4
+                 \
+                  5
+                   \
+                    6
+
+ //将原来的右子树接到左子树的最右边节点
+    1
+     \
+      2
+       \
+        3
+         \
+          4
+           \
+            5
+             \
+              6
+
+作者：windliang
+链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by--26/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
